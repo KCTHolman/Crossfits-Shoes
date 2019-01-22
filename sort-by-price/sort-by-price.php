7a505eab87c5a28cec6bc78b0e9b01666aafd6df
KCTHolman <46706715+KCTHolman@users.noreply.github.com>
2019-01-22 15:04:23 +0100

    sorteren op prijs

--- sort-by-price/sort-by-price.php
+++ sort-by-price/sort-by-price.php
@@ -1,5 +1,5 @@
 <?php
-    include 'includes/db_connection.php';
+    include '../includes/db_connection.php';
     ?>
 
 
@@ -9,16 +9,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="index.css" type="text/css" rel="stylesheet">
+    <link href="../index.css" type="text/css" rel="stylesheet">
     <title>Crossfit shoes</title>
 </head>
 <body>
     <div class="container1">
-        <div class="box1"> 
-           <a href="sort-by-price/sort-by-price.php">sorteren op prijs</a></div>
+        <div class="box1"></div>
         <div class="box2">
-
-        <?php include "includes/cards.php"; ?> <!-- sort-by-price.php of cards.php -->
+        <?php include "../includes/sort-by-price.php"; ?> <!-- sort-by-price.php of cards.php -->
         </div>
 
         <div class="box3"></div>
